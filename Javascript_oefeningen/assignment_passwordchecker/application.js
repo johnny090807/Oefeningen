8be40af097560e85a942ce82516287a8d5820b34
john klees <johnnynl@live.nl>
2018-10-01 16:17:11 +0200

    feedback verwerkt in assignment_password-checker

--- Javascript_oefeningen/assignment_passwordchecker/application.js
+++ Javascript_oefeningen/assignment_passwordchecker/application.js
@@ -1,53 +1,133 @@
 let userName  = document.getElementById("login__username");
 let passWord  = document.getElementById("login__password");
-function checkForm(){
-    if(userName.value == ""){
-    alert("Je moet wel je username invullen!");
-    userName.focus();
-    return false;
+    
+      
+    function checkForm(){
+        let passwordArray = passWord.value.split('');
+        let specialeKarakters = ["!","@","#","$","%","^","&"];
+        let passwordNumbers = 0;
+        let passwordSpecialCharacter = 0;
+        let passwordLowerCase = 0;
+        let passwordUpperCase = 0;
+        var lowerCaseArray =[]; 
+        let x;
+        let y;
+
+        for (var index='a'.charCodeAt(0),end='z'.charCodeAt(0); 
+            index <=end; ++index)
+            {lowerCaseArray.push(String.fromCharCode(index));} lowerCaseArray.join();
+
+
+        for (x = 0; x < passwordArray.length; x++){
+            for(y = 0; y < 10; y++){
+                if (y == passwordArray[x]){
+                    passwordNumbers++;
+                }   
+                
+            }
+        }
+        for (x = 0; x < passwordArray.length; x++){
+            for(y = 0; y < lowerCaseArray.length; y++){
+                if (lowerCaseArray[y] == passwordArray[x]){
+                    passwordLowerCase++;
+                }   
+                
+            }
+        }
+        for (x = 0; x < passwordArray.length; x++){
+            for(y = 0; y < lowerCaseArray.length; y++){
+                if (lowerCaseArray[y].toUpperCase() == passwordArray[x]){
+                    passwordUpperCase++;
+                }   
+                
+            }
+        }
+        for (x = 0; x < passwordArray.length; x++){
+            for(y = 0; y < specialeKarakters.length; y++){
+                if (specialeKarakters[y] == passwordArray[x]){
+                    passwordSpecialCharacter++;
+                }   
+                
+            }
+        }
+
+        if(userName.value == ""){
+            showError("Je moet wel je username invullen!");
+            userName.focus();
+            return false;
+        }
+        if(passWord.value == ""){
+            showError("Je moet wel je wachtwoord invullen!");
+            passWord.focus();
+            return false;
+        }        
+        if(passWord.value.length < 8){
+            showError("Wachtwoord moet minstens 8 karakters hebben!");
+            passWord.focus();
+            return false;        
+        }
+        if(passwordLowerCase < 1){
+            showError("Het wachtwoord moet 1 lowercase karakter hebben!");
+            passWord.focus();
+            return false;
+        }
+        if(passwordUpperCase < 1){
+            showError("Het wachtwoord moet 1 uppercase karakter hebben!");
+            passWord.focus();
+            return false;
+        }
+        if(passwordNumbers < 2){
+            showError("Het wachtwoord moet meer dan 2 nummers bevatten!");
+            passWord.focus();
+            return false;
+        }
+        if(passwordSpecialCharacter < 1){
+            showError("Het wachtwoord moet 1 speciaal karakter hebben!");
+            passWord.focus();
+            return false;
+        }
+        showSuccess("Je hebt een valide wachtwoord gegeven!");
+       
     }
 
-    re = /^\w+$/;
-    if(!re.test(userName.value)) {
-    alert("Error: Username must contain only letters, numbers and underscores!");
-    form.username.focus();
-    return false;
-    }
-
-    if(passWord.value.length < 8){
-    alert("Error: Password moet minstens 8 karakters hebben!");
-    passWord.focus();
-    return false;        
-    }
-
-    re = /[0-9]{2,}/;
-    if(!re.test(passWord.value)) {
-    alert("Error: Password moet minstens 2 nummers hebben!");
-    passWord.focus();
-    return false;
-    }
-
-    re = /[a-z]/;
-    if(!re.test(passWord.value)) {
-    alert("Error: Je wachtwoord moet minstens 1 lowercase letter hebben!");
-    passWord.focus();
-    return false;
-    }
-
-    re = /[A-Z]/;
-    if(!re.test(passWord.value)) {
-    alert("Error: Je wachtwoord moet minstens 1 uppercase letter hebben!");
-    passWord.focus();
-    return false;
-    }
-
-    re = /[!@#$%^&*]/;
-    if(!re.test(passWord.value)) {
-    alert("Error: Je wachtwoord moet minstens een speciale karakter hebben!");
-    passWord.focus();
-    return false;
-    }
-
-    alert("Je hebt een valide wachtwoord gegeven!");
-    return true;
+// SHOW ERROR OR SUCCESS //
+function showError(string){
+    let errorDiv = document.getElementById("login__error");
+    errorDiv.innerHTML = "";
+    let template = `
+            <div class="login__errormsg">
+                <p>${string}</p>
+            </div>
+            `;
+    errorDiv.insertAdjacentHTML('beforeend', template);
 }
+function showSuccess(string){
+    let errorDiv = document.getElementById("login__error");
+    errorDiv.innerHTML = "";
+    let template = `
+            <div class="login__successmsg">
+                <p>${string}</p>
+            </div>
+            `;
+    errorDiv.insertAdjacentHTML('beforeend', template);
+}
+
+// ON ENTER START CHECKER //
+userName.addEventListener("keyup", function(event) {
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("login__button").click();
+  }
+});
+passWord.addEventListener("keyup", function(event) {
+  // Cancel the default action, if needed
+  event.preventDefault();
+  // Number 13 is the "Enter" key on the keyboard
+  if (event.keyCode === 13) {
+    // Trigger the button element with a click
+    document.getElementById("login__button").click();
+  }
+});
