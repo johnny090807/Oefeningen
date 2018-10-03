155deb29d6feea479b9d03a5456eba391c95059e
john klees <johnnynl@live.nl>
2018-10-03 09:32:12 +0200

    Feedback oefening 1 verwerkt.

--- Javascript_oefeningen/assignment_addressbook/application.js
+++ Javascript_oefeningen/assignment_addressbook/application.js
@@ -1,24 +1,68 @@
-
-const url = 'https://randomuser.me/api/?nationality=nl&results=20';
+const url = 'https://randomuser.me/api/?nationality=nl&results=10';
+let allUsers;
+let singleUser;
 
 fetch(url)
 .then((response) => response.json())
 .then(function(data) {
-	let users = data.results;
-		return users.map(function(user){
-		console.log(user);
-			var main_div = document.getElementById("main");
-
-			let template = `
-			<img src="${user.picture.large}" />
-			<p class="name">
-				${user.name.first} ${user.name.last}<br />
-				${user.cell}
-			</p>
-			`
-
-			main_div.insertAdjacentHTML('beforeend', template);
-		});
-
-	
+	allUsers = data.results;
+	fillDiv(allUsers);
 });
+
+function fillDiv(users){
+	return users.map(function(user){
+		const main_div = document.getElementById("main");
+		let template = `
+		<div class="usersholder" onclick="getDetail(this)">
+			<img src="${user.picture.large}" class="usersholder__image"/>
+				<p class="name">
+					${user.name.first} ${user.name.last}<br />
+					${user.cell}
+			</p>
+		</div>
+		`
+		main_div.insertAdjacentHTML('beforeend', template);
+	});
+}
+function fillUser(user){
+		const main_div = document.getElementById("main");
+		console.log(user);
+		let template = `
+		<div class="userholder" onclick="closeDetail()">
+		<h1>Click to go back:</h1>
+			<img src="${user.picture.large}" class="userholder__image"/>
+				<p class="name">
+				<p>${user.name.first}</p> 
+				<p>${user.name.last}</p> 
+				<p>${user.cell}</p> 
+				<p>${user.dob.age}</p> 
+				<p>${user.dob.date}</p> 
+				<p>${user.email}</p> 
+				<p>${user.gender}</p> 
+				<p>${user.id.name}${user.id.value}</p> 
+				<p>${user.location.city}</p> 
+				<p>${user.location.state}</p> 
+				<p>${user.location.street}</p> 
+				<p>${user.login.username}</p> 
+				<p>${user.login.password}</p> 
+				<p>${user.registered.date}</p> 
+			</p>
+		</div>
+		`
+		main_div.insertAdjacentHTML('beforeend', template);
+}
+function getDetail(user){
+	let main_div = document.getElementById("main");
+	let main_image = user.querySelector(".usersholder__image").src;
+	main_div.innerHTML = "";
+	allUsers.forEach( function(element, index) {
+		if(element.picture.large == main_image){
+			fillUser(element);
+		}
+	});
+}
+function closeDetail(){
+	let main_div = document.getElementById("main");
+	main_div.innerHTML = "";
+	fillDiv(allUsers);
+}
